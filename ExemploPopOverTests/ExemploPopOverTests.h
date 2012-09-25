0dfcffe1f439e809c5e57fbbc83ccce815527287
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-25 11:36:59 -0300

    Initial Commit

---
+++ ExemploPopOverTests/ExemploPopOverTests.h
@@ -0,0 +1,13 @@
+//
+//  ExemploPopOverTests.h
+//  ExemploPopOverTests
+//
+//  Created by Rafael Brigagão Paulino on 25/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ExemploPopOverTests : SenTestCase
+
+@end
---
+++ ExemploPopOverTests/ExemploPopOverTests.m
@@ -0,0 +1,32 @@
+//
+//  ExemploPopOverTests.m
+//  ExemploPopOverTests
+//
+//  Created by Rafael Brigagão Paulino on 25/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "ExemploPopOverTests.h"
+
+@implementation ExemploPopOverTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ExemploPopOverTests");
+}
+
+@end
